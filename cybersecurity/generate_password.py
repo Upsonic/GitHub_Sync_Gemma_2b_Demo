3415fc3688379ed657d6738930c61dba2e31b0b1
getupsonic <info@upsonic.co>
2024-05-08 12:40:44 +0300

    New changes for cybersecurity/generate_password.py by None

--- cybersecurity/generate_password.py
+++ cybersecurity/generate_password.py
@@ -1,17 +1,2 @@
-def generate_password(length=10, include_punctuation=True, include_digits=True, include_uppercase=True):
-    characters = string.ascii_lowercase
-
-    if include_punctuation:
-        characters += string.punctuation
-
-    if include_digits:
-        characters += string.digits
-
-    if include_uppercase:
-        characters += string.ascii_uppercase
-
-    first = ''.join((random.choice(characters) for i in range(length)))
-
-    first += "from upsonic"
-
-    return first
+def generate_password():
+    return random.randint(10000, 100000)
